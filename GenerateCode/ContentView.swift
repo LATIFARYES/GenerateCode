94f9299f704153440ec77f3cbd2fddb0670e3cd6
LATIFARYES <latifaryes@gmail.com>
2024-05-20 14:07:34 +0300

    Numbers6

--- GenerateCode/ContentView.swift
+++ GenerateCode/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            NavigationLink(destination: MyView()) {
+                Text("Link to MyView")
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct MyView: View {
+    
+    @State private var randomString: String = ""
+    
+    var body: some View {
+        VStack {
+            Text(randomString)
+                .padding()
+            
+            Button("Generate Code") {
+                randomString = randomStringWithLength(len: 6) as String
+            }
+            .padding()
+        }
+        .navigationTitle("Generated Code")
+    }
+    
+    func randomStringWithLength(len: Int) -> String {
+        let letters = "0123456789"
+        var randomString = ""
+        
+        for _ in 0..<len {
+            let randomIndex = Int(arc4random_uniform(UInt32(letters.count)))
+            let randomCharacter = letters[letters.index(letters.startIndex, offsetBy: randomIndex)]
+            randomString.append(randomCharacter)
+        }
+        
+        return randomString
+    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
