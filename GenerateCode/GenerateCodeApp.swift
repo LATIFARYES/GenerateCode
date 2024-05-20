9d3bf79a494b51c2ad5d77175a606791d270f1c5
LATIFARYES <latifaryes@gmail.com>
2024-05-20 14:04:52 +0300

    Initial Commit

---
+++ GenerateCode/GenerateCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerateCodeApp.swift
+//  GenerateCode
+//
+//  Created by LATIFA on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GenerateCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
